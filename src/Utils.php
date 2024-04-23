c2bdce78f33e87c1e083c822aebd1b1937014e26
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2024-04-23 09:32:03 -0300

    Added calculateFormulas

--- src/Utils.php
+++ src/Utils.php
@@ -154,4 +154,30 @@ class Utils
     public static function isNullOrEmpty($str): bool{
         return ($str === null || trim($str) === '');
     }
+
+    /**
+     * Calculates formulas with replaceable variables
+     *
+     * @param string $formula The formula can consist of mathematic operations and variables
+     * @param array $variables Variables are made out of a key which will be inside the formula and will be replaced and a value which will be the value the variable in the formula will be replaced
+     * @return int|float|false|null Returns a numeric value of the result of the formula, false if an exception was thrown or null if the formula didn't return a numeric value
+     */
+    public static function calculateFormulas(string $formula, array $variables)
+    {
+        $result = floatval($formula);
+        if (is_numeric($result)) {
+            return $result;
+        }
+        
+        foreach($variables as $var => $val){
+            $formula = str_replace($var, $val, $formula);
+        }
+        
+        try {
+            $evaluated = eval('return ('.$formula.');');
+            return is_numeric($evaluated) ? $evaluated : null;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 }
